db8abf13b1c37c9e26949822c5c6febbb0b18d8b
Vasu Dev Arora <33614620+vasudevarora1996@users.noreply.github.com>
2021-01-04 19:22:37 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num=int(input("ENTER A NUMBER TO CALCULATE FACTORIAL"))
+def factorial(num):
+    fact=1
+    for i in range(num,1,-1):
+        fact = fact * i
+    print("FACTORIAL OF",num,"IS",fact)    
+
+factorial(num)
