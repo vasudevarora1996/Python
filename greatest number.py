58ed53e257f243ec9600dedd7b575dbd3771cc8e
Vasu Dev Arora <33614620+vasudevarora1996@users.noreply.github.com>
2021-01-04 19:16:13 +0530

    Create greatest number.py

---
+++ greatest number.py
@@ -0,0 +1,7 @@
+first=int(input("Enter first number"))
+second=int(input("Enter second number"))
+print("MAXIMUM OF TWO GIVEN NUMBERS IS ")
+if first>second:
+    print("MAXIMUM NUMBER IS",first)
+else:
+    print("MAXIMUM NUMBER IS",second)
