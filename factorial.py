bdd5730af8138de001ae0a8d8d19a9978882567c
Vasudev Arora <vasudevarora@vasudev.local>
2021-01-12 11:28:34 +0530

    changes in factorial file

--- factorial.py
+++ factorial.py
@@ -3,6 +3,6 @@ def factorial(num):
     fact=1
     for i in range(num,1,-1):
         fact = fact * i
-    print("FACTORIAL OF",num,"IS",fact)    
+    print("FACTORIAL OF {} is {}".format(num,fact))    
 
 factorial(num)
