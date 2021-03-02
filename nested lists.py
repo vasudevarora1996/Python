e5cb4514ed49f0742c6752fc6f6524df9f290750
Vasu Dev Arora <33614620+vasudevarora1996@users.noreply.github.com>
2021-03-02 21:12:40 +0530

    Nested Lists HackerRank Solution

---
+++ nested lists.py
@@ -0,0 +1,17 @@
+'''Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.'''
+
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+
+    students = sorted(students, key = lambda x: x[1])
+    second_lowest_score = sorted(list(set([x[1] for x in students])))[1]
+    desired_students = []
+    for stu in students:
+        if stu[1] == second_lowest_score:
+            desired_students.append(stu[0])
+    print("\n".join(sorted(desired_students)))
